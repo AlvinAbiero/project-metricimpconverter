1e456344c0dfd45546f88b4d13d2520c98200e58
AlvinAbiero <abieroalvin@gmail.com>
2025-04-08 11:04:55 +0300

    feat: finished all functional test and made them pass

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -42,4 +42,147 @@ suite("Unit Tests", function () {
       done();
     });
   });
+
+  suite("Function convertHandler.getUnit(input)", function () {
+    test("For Each Valid Unit Inputs", function (done) {
+      const input = [
+        "gal",
+        "l",
+        "mi",
+        "km",
+        "lbs",
+        "kg",
+        "GAL",
+        "L",
+        "MI",
+        "KM",
+        "LBS",
+        "KG",
+      ];
+
+      const expected = [
+        "gal",
+        "L",
+        "mi",
+        "km",
+        "lbs",
+        "kg",
+        "gal",
+        "L",
+        "mi",
+        "km",
+        "lbs",
+        "kg",
+      ];
+
+      input.forEach(function (ele, index) {
+        assert.equal(convertHandler.getUnit(ele), expected[index]);
+      });
+      done();
+    });
+
+    test("Unknown Unit Input", function (done) {
+      const input = "32gram";
+      assert.isNull(convertHandler.getUnit(input));
+      done();
+    });
+  });
+
+  suite("Function convertHandler.getReturnUnit(initUnit)", function () {
+    test("For each Valid Unit Inputs", function (done) {
+      const input = ["gal", "L", "mi", "km", "lbs", "kg"];
+      const expected = ["L", "gal", "km", "mi", "kg", "lbs"];
+
+      input.forEach(function (ele, index) {
+        assert.equal(convertHandler.getReturnUnit(ele), expected[index]);
+      });
+      done();
+    });
+  });
+
+  suite("Function convertHandler.spellOutUnit(unit)", function () {
+    test("For Each Valid Unit Inputs", function (done) {
+      const input = ["gal", "L", "mi", "km", "lbs", "kg"];
+      const expected = [
+        "gallons",
+        "liters",
+        "miles",
+        "kilometers",
+        "pounds",
+        "kilograms",
+      ];
+      input.forEach(function (ele, index) {
+        assert.equal(convertHandler.spellOutUnit(ele), expected[index]);
+      });
+      done();
+    });
+  });
+
+  suite("Function convertHandler.convert(num, unit)", function () {
+    test("Gal to L", function (done) {
+      const input = [5, "gal"];
+      const expected = 18.9271;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+
+    test("L to Gal", function (done) {
+      const input = [5, "L"];
+      const expected = 1.32086;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+
+    test("Mi to Km", function (done) {
+      const input = [5, "mi"];
+      const expected = 8.0467;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+
+    test("Km to Mi", function (done) {
+      const input = [5, "km"];
+      const expected = 3.10686;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+
+    test("Lbs to Kg", function (done) {
+      const input = [5, "lbs"];
+      const expected = 2.26796;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+
+    test("Kg to Lbs", function (done) {
+      const input = [5, "kg"];
+      const expected = 11.02312;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
+    });
+  });
 });
